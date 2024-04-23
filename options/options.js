7b55c481f51ef4614723d7acd207ded662a5cc7e
Crowdbotics <team@crowdbotics.com>
2024-04-23 16:17:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-mouse-47676",
+  url: "https://delicate-mouse-47676.botics.co",
+  api: "https://delicate-mouse-47676.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
